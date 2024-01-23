87c151d4d31453cda41c2f111bc7cfd73a86adb6
tigerhu <huxy99@yahoo.com>
2024-01-22 20:48:57 -0600

    Change the inbound port

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         admin.create_admin_user()
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5001)
