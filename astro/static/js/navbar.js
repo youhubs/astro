db2d6b613fe5d62455bbf455b5b54f2fa553643c
tigerhu <huxy99@yahoo.com>
2024-01-23 20:52:38 -0600

    Update the Mobile menu

--- astro/static/js/navbar.js
+++ astro/static/js/navbar.js
@@ -1,7 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Toggle the nav menu visibility on click
-    document.getElementById('menuToggle').addEventListener('click', function () {
-        const navMenuContainer = document.querySelector('.nav-menu-container');
-        navMenuContainer.style.display = navMenuContainer.style.display === 'block' ? 'none' : 'block';
-    })
+document.addEventListener("DOMContentLoaded", function () {
+  // Toggle the nav menu visibility on click
+  document.getElementById("mobile-menu").addEventListener("click", function () {
+    const navMenuContainer = document.querySelector(".nav-menu-container");
+    navMenuContainer.style.display =
+      navMenuContainer.style.display === "block" ? "none" : "block";
+  });
 });
